818df9d71be7d4f942309ff33819cdd558d4417e
Bart Merenda <outsbart@gmail.com>
2017-10-30 16:10:18 +0100

    Added sdl2 dependency

--- src/bin/gameman.rs
+++ src/bin/gameman.rs
@@ -2,6 +2,12 @@
 #![allow(unused_mut)]
 
 extern crate gameman;
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
 use gameman::utils::load_boot_rom;
 use gameman::cpu::CPU;
 use gameman::mem::MMU;
@@ -39,7 +45,54 @@ fn main() {
     memory.set_bios(load_boot_rom());
     let mut cpu = CPU::new(memory);
 
-    for _ in 0..1000000 {
-        cpu.step();
-    }
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("rust-sdl2 demo: Window", 800, 600)
+        .resizable()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+
+    let mut tick = 0;
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
+                _ => {}
+            }
+        }
+
+        {
+            // Update the window title.
+            let window = canvas.window_mut();
+
+            let position = window.position();
+            let size = window.size();
+            let title = format!("Window - pos({}x{}), size({}x{}): {}",
+                                position.0,
+                                position.1,
+                                size.0,
+                                size.1,
+                                tick);
+            window.set_title(&title).unwrap();
+
+            tick += 1;
+            cpu.step();
+        }
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+        canvas.present();
+}
+
+//    for _ in 0..1000000 {
+//        cpu.step();
+//    }
 }
