8569b1bd52bfd28a0e0def64a4b89e0e8c72e14b
Bart Merenda <outsbart@gmail.com>
2019-09-06 11:37:14 +0200

    Implemented Square Channel register 1

--- src/sound/mod.rs
+++ src/sound/mod.rs
@@ -10,6 +10,26 @@ pub struct Sound {
 
 impl Memory for Sound {
     fn read_byte(&mut self, addr: u16) -> u8 {
+//        match addr & 0xff {
+//            0x10 => self.channel_1.sweep.read(),
+//            0x11 => self.channel_1.read_reg1(),
+//            0x12 => self.channel_1.envelope.read_reg(),
+//            0x14 => self.channel_1.read_reg4(),
+//            0x16 => self.channel_2.read_reg1(),
+//            0x17 => self.channel_2.envelope.read_reg(),
+//            0x19 => self.channel_2.read_reg4(),
+//            0x1a => self.channel_3.read_reg0(),
+//            0x1c => self.channel_3.read_reg2(),
+//            0x1e => self.channel_3.read_reg4(),
+//            0x21 => self.channel_4.envelope.read_reg(),
+//            0x22 => self.channel_4.read_reg3(),
+//            0x23 => self.channel_4.read_reg4(),
+//            0x24 => self.get_ctrl_volume(),
+//            0x25 => self.get_terminal_channels(),
+//            0x26 => self.get_ctrl_master(),
+//            0x30...0x3f => self.channel_3.read_wave_ram(addr - 0xff30),
+//            _ => 0xff,
+//        }
         0
     }
 
@@ -73,14 +93,28 @@ impl Sweep {
 struct SquareChannel {
     pub sweep: Sweep,
     pub envelope: Envelope,
+
+    pub duty: u8,
+    pub length_load: u8,
 }
 
 impl SquareChannel {
     pub fn new() -> Self {
         SquareChannel {
             sweep: Sweep::new(),
-            envelope: Envelope::new()
+            envelope: Envelope::new(),
+            duty: 0,
+            length_load: 0
         }
+    }
+
+    pub fn write_register_1(&mut self, byte: u8) {
+        self.length_load = byte & 0b0011_1111;
+        self.duty = (byte & 0b1100_0000) >> 6;
+    }
+
+    pub fn read_register_1(&self) -> u8 {
+        (self.duty << 6) | self.length_load
     }
 }
 
@@ -124,6 +158,22 @@ mod tests {
         sweep.time = 0b100;
 
         assert_eq!(sweep.read(), 0b0100_0010);
+    }
+
+    #[test]
+    fn test_square_register_1() {
+        let mut channel: SquareChannel = SquareChannel::new();
+
+        assert_eq!(channel.read_register_1(), 0);
+
+        channel.write_register_1(0b1000_1111);
+        assert_eq!(channel.length_load, 0b1111);
+        assert_eq!(channel.duty, 0b10);
+
+        channel.length_load = 0b1110;
+        channel.duty = 0b11;
+
+        assert_eq!(channel.read_register_1(), 0b1100_1110);
     }
 
 }
