087eaf02188772ca06cd4fa9f78d1348b3dbb0cf
bart <outsbart@gmail.com>
2025-05-12 22:34:00 +0200

    Run more mooneye timer tests

--- tests/test_mooneye_timer_roms.rs
+++ tests/test_mooneye_timer_roms.rs
@@ -13,3 +13,21 @@ fn tim00() {
     let mut emulator = Gameboy::new("tests/timer/tim00.gb");
     assert!(emulator.passes_mooneye_test_rom());
 }
+
+#[test]
+fn tim01() {
+    let mut emulator = Gameboy::new("tests/timer/tim01.gb");
+    assert!(emulator.passes_mooneye_test_rom());
+}
+
+#[test]
+fn tim10() {
+    let mut emulator = Gameboy::new("tests/timer/tim10.gb");
+    assert!(emulator.passes_mooneye_test_rom());
+}
+
+#[test]
+fn tim11() {
+    let mut emulator = Gameboy::new("tests/timer/tim11.gb");
+    assert!(emulator.passes_mooneye_test_rom());
+}
